2609c5351090c412c8f0408d8dc0920c42c3e91c
Irelia <yaokai729@outlook.com>
2019-12-05 12:33:27 +0800

    修改.gitignore

--- src/app2.js
+++ src/app2.js
@@ -3,13 +3,15 @@ import $ from 'jquery';
 
 const $tabBar = $('#app2 .tab-bar')
 const $tabContent = $('#app2 .tab-content')
+let index = localStorage.getItem('localKey') || 0
 
 $tabBar.on('click', 'li', (e) => {
   const $li = $(e.currentTarget)
   $li.addClass('selected').siblings().removeClass('selected')
-  const index = $li.index()
+  index = $li.index()
+  localStorage.setItem('localKey', index)
   $tabContent.children().eq(index).addClass('active')
     .siblings().removeClass('active')
 })
 
-$tabBar.children().eq(0).trigger('click')
+$tabBar.children().eq(index).trigger('click')
