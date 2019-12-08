b5bd8dbd3d1101ee532a0135497f78ee66f7a2f5
Irelia <yaokai729@outlook.com>
2019-12-08 12:54:53 +0800

    使用MVC思想优化代码

--- src/app3.js
+++ src/app3.js
@@ -1,7 +1,31 @@
-import './app3.css'
-import $ from 'jquery'
+import "./app3.css";
+import $ from "jquery";
 
-const $square = $('#app3 .square')
-$square.on('click', () => {
-  $square.toggleClass('active')
-})
+const html = `
+  <section id="app3">
+    <div class="square"></div>
+  </section>
+`
+$(html).appendTo($('body>.page'))
+
+const $square = $("#app3 .square");
+const localKey = "app3.active";
+let active = localStorage.getItem(localKey) === "yes";
+
+// if (active) {
+//   $square.addClass("active");
+// } else {
+//   $square.removeClass("active");
+// }
+
+$square.toggleClass("active", active);
+
+$square.on("click", () => {
+  if ($square.hasClass("active")) {
+    $square.removeClass("active");
+    localStorage.setItem(localKey, "no");
+  } else {
+    $square.addClass("active");
+    localStorage.setItem("app3.active", "yes");
+  }
+});
